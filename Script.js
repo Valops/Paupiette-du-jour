aa4a954a83e572b63150a8d4107c970548ed169e
Valops <79655913+Valops@users.noreply.github.com>
2026-04-22 09:31:05 +0200

    Add file web upload to create a project
    Add index.html; Paupiette.json; Script.js, style.CSS, folder Image for content

---
+++ Script.js
@@ -0,0 +1,26 @@
+let paupiettes = [];
+
+fetch("paupiette.json")
+.then(res => res.json())
+.then(data => {
+    paupiettes = data;
+    paupietteDuJour();
+});
+
+function paupietteDuJour() {
+    const today = new Date();
+    const index = today.getDate() % paupiettes.length;
+    afficher(paupiettes[index]);
+}
+
+function randomPaupiette() {
+    const index = Math.floor(Math.random() * paupiettes.length);
+    afficher(paupiettes[index]);
+}
+
+function afficher(p) {
+    document.getElementById("nom").innerText = p.nom;
+    document.getElementById("desc").innerText = p.description;
+    document.getElementById("auteur").innerText = "par " + p.auteur;
+    document.getElementById("image").src = p.image;
+}
